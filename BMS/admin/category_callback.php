3d381173e1edebe17bc3be095a9fa3d30e2cf4d7
Gourav Rohilla <gourav33rohilla@gmail.com>
2023-12-20 15:47:53 +0530

    changed in category_callback

--- BMS/admin/category_callback.php
+++ BMS/admin/category_callback.php
@@ -5,7 +5,7 @@ if(isset($_POST['name']) && trim($_POST['name'])!=''){
     $title = $_POST['name'];
     $detail = $_POST['description'];
 
-    echo $query="insert into category(name,detal) values('$title','$detail')";
+    echo $query="insert into category(name,detail) values('$title','$detail')";
     $result = $conn->query($query);
     
 
